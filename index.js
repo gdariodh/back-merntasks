7215a40e53f5406aa39b252982464693774065d7
gdariodh <diazhernandezgabriel2001@gmail.com>
2020-08-18 18:25:19 -0400

    ajustes

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/proyectos", require("./routes/proyectos"));
 app.use("/api/tareas",require("./routes/tareas"));
 // arracar la app
-app.listen(port, '0.0.0.0',() => {
+app.listen(port, '0.0.0.0/0',() => {
   console.log(`El servidor esta corriendo en el puerto ${port}`);
 });
