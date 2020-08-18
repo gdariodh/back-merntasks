9d1ee32887361f101bbfe01f1ea418f52f49ede3
gdariodh <diazhernandezgabriel2001@gmail.com>
2020-08-18 17:44:27 -0400

    ajustes para deployment

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ app.use(cors());
 // habilitar express.json - otra forma bodyparser
 app.use(express.json({ extended: true }));
 // puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 // importar rutas
 app.use("/api/usuarios", require("./routes/usuarios"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/proyectos", require("./routes/proyectos"));
 app.use("/api/tareas",require("./routes/tareas"));
 // arracar la app
-app.listen(PORT, () => {
-  console.log(`El servidor esta corriendo en el puerto ${PORT}`);
+app.listen(port, '0.0.0.0',() => {
+  console.log(`El servidor esta corriendo en el puerto ${port}`);
 });
